732b37981c323eb421bb742f0a9ad952fbe02aec
tbergq <trond_bergquist@hotmail.com>
2017-02-24 12:33:01 +0100

    Finish up add player detail page

--- src/client/app/pages/PlayerDetailPage/PlayerDetailPage.js
+++ src/client/app/pages/PlayerDetailPage/PlayerDetailPage.js
@@ -11,18 +11,28 @@ import {
   Table,
   Glyphicon
 } from 'react-bootstrap';
+import DatePicker from 'react-bootstrap-date-picker';
+import styles from './PlayerDetail.scss';
 
 @observer
 class PlayerDetailPage extends React.Component {
 
-  @observable player = null;
+  @observable player     = null;
+  @observable changeDate = new Date().toISOString();
+  @observable dateFormat = 'DD/MM/YYYY';
 
   constructor(props) {
     super(props);
+
+    this.dateChanged = this.dateChanged.bind(this);
+    this.changeValue = this.changeValue.bind(this);
   }
 
   @action
   async componentWillMount() {
+    if (navigator.language === 'nb') {
+      this.dateFormat = 'DD.MM.YYYY';
+    }
     try {
       this.player = await PlayerStore.getPlayer(this.props.routeParams.teamId, this.props.routeParams.playerId);
     }
@@ -31,11 +41,32 @@ class PlayerDetailPage extends React.Component {
     }
   }
 
+  @action
+  changeValue(key, increment) {
+    let value = increment ? 1 : -1;
+
+    if (increment && this.player[key] === 20) {
+      ToastStore.addToastMessage(`Player's skill is already at max value`);
+      return;
+    }
+    else if (!increment && this.player[key] === 0) {
+      ToastStore.addToastMessage(`Player's skill is already at minimum value`);
+      return;
+    }
+
+    this.player[key] += value;
+  }
+
+  @action
+  dateChanged(value) {
+    this.changeDate = value;
+  }
+
   render() {
     if (!this.player) {
       return <div className="container">...loading</div>
     }
-    console.log('render', JSON.stringify(this.player));
+
     return (
       <div className="container">
         <h2>{this.player.name}</h2>
@@ -43,12 +74,16 @@ class PlayerDetailPage extends React.Component {
           <thead>
           <tr>
             <th colSpan="4">
-              <Button onClick={() => {
-                browserHistory.goBack()
-              }}
-              >
-                Back
-              </Button>
+              <div className="pull-left">
+                <label>Change date</label>
+                <DatePicker
+                  value={this.changeDate}
+                  onChange={this.dateChanged}
+                  dateFormat={this.dateFormat}
+                  weekStartsOnMonday={true}
+                  className={styles.datePicker}
+                />
+              </div>
             </th>
           </tr>
           <tr>
@@ -67,11 +102,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('keeper', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('keeper', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -85,11 +122,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('defending', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('defending', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -103,11 +142,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('playmaking', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('playmaking', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -121,11 +162,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('winger', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('winger', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -139,11 +182,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('passing', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('passing', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -157,11 +202,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('scoring', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('scoring', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -175,11 +222,13 @@ class PlayerDetailPage extends React.Component {
               <Button
                 bsStyle="success"
                 bsSize="small"
+                onClick={() => this.changeValue('set_pieces', true)}
               >
                 <Glyphicon glyph="plus"/>
               </Button>
               <Button
                 bsSize="small"
+                onClick={() => this.changeValue('set_pieces', false)}
               >
                 <Glyphicon glyph="minus"/>
               </Button>
@@ -187,6 +236,19 @@ class PlayerDetailPage extends React.Component {
           </tr>
           </tbody>
         </Table>
+        <div className="pull-right">
+          <Button onClick={() => {
+            browserHistory.goBack()
+          }}
+          >
+            Back
+          </Button>
+          <Button
+            bsStyle="primary"
+          >
+            Save
+          </Button>
+        </div>
       </div>
     )
   }
